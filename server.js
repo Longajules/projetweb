a9b82ad6ee56c1dbac2dfc2d0bea482def06e65d
Longajules <judonite60@gmail.com>
2026-01-16 11:52:43 +0100

    miseenplacedesql

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express'); // var expresse prend expresse pour le http
+const app = express(); // instasie expresse
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: '172.29.18.119',
+    user: 'userServerNode',
+    password: 'userServerNode',
+    database: 'bddesh'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Erreur de connexion  a la base de données :', err);
+        return;
+    }
+    console.log('connectea a la base de donnees MySQL');
+});
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/login', (req, res) => { // C'est une route type get donc par navigateur
+    res.send('page login');
+});
+
+app.get('/info', (req, res) => { // C'est une route mais type "get" donc que par formulaire
+    res.json({ cle1: 'crée compte', cle2: '' }); // Requet json mieux compri par navigateur
+});
+
+app.post('/register', (req, res) => { // C'est une route mais type "post" donc que par formulaire
+    console.log('Données reçues pour l\'inscription');
+    console.log(req.body);
+    res.send('crée compte');
+});
+
+app.listen(3000, () => { //express écoute sur le port 3000 et affiche un message dans le console
+    console.log('server runing')
+});  //Le poind virgule c'est juste pour dire la fin de la fonction
